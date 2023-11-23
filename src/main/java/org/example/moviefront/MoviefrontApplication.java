5a727c3533d3927aa3b49ba132b15aedba4786af
matthcol <matthias.colin@gmail.com>
2023-11-23 13:30:58 +0100

    first commit

---
+++ src/main/java/org/example/moviefront/MoviefrontApplication.java
@@ -0,0 +1,13 @@
+package org.example.moviefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviefrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/moviefront/MoviefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moviefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
